d05e71b0a7ce935deea463490cbe1939c0c299e0
khushboo-590 <khushbook4979@gmail.com>
2025-01-20 09:56:46 +0530

    victorian resistance

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.silder').slick({
+        // infinite: true,
+        // speed: 50,
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+       
+
+    });
+});
